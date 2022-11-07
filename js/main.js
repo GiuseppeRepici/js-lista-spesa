82eaa8e1c510da533a0668397b749a9a53046d8f
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-07 15:46:47 +0100

    completed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const listaSpesa = ["pane" , "pasta" , "acqua" , "eccecc"] ;
+const contain = document.getElementById("container");
+let i = 0;
+
+while (i < listaSpesa.length) {
+    contain.innerHTML += " " + listaSpesa[i] + " " ;
+    i++;
+}
